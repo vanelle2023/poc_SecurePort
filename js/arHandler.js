9707f72f4706dec5731a9b32d20329d5c3bc7811
vanelle2023 <131911226+vanelle2023@users.noreply.github.com>
2025-11-12 17:25:26 +0100

    Update arHandler.js

--- js/arHandler.js
+++ js/arHandler.js
@@ -17,12 +17,7 @@ export function setupAR(app) {
   // Reticle erstellen (grüner Kreis)
   {
     const geometry = new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2);
-    const material = new THREE.MeshBasicMaterial({
-      color: 0x00ff00,
-      side: THREE.DoubleSide,
-      transparent: true,
-      opacity: 0.8
-    });
+    const material = new THREE.MeshBasicMaterial();
     reticle = new THREE.Mesh(geometry, material);
     reticle.matrixAutoUpdate = false;
     reticle.visible = false;
